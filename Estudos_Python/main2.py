0f8139bed34278a9b4a263374c376f2b1137ad5a
CharlezAlvez <charlesalvez132@gmail.com>
2026-07-15 15:49:40 -0300

    Todos os Projetos

--- Estudos_Python/main2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-from pymysql import create_engine 
-caminho_csv = pd.read_csv("C:\Users\davyd\Downloads\archive\exec_orc_estados")
-df = pd.read_csv(caminho_csv)
-df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
-print(caminho_csv)
